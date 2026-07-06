1665d80750daa6dd9707d96b062323c54fbd8e6d
Emine907 <emineero61@gmail.com>
2026-07-06 14:51:20 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$sunucu = "localhost";
-$kullanici = "root";
-$sifre = "";
+$sunucu = "sql301.infinityfree.com ";
+$kullanici = "f0_42348949";
+$sifre = "69738565";
 $veritabani = "todo_db";
 
 //Veritabanı bağlantısı
